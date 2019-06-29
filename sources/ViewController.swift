f0fd59280a7d685096b237ab46582b89a3fc1d74
阿美 <ameijing@baohaishuodeMac-mini.local>
2019-06-29 10:31:20 +0800

    feat: add get request test

--- sources/ViewController.swift
+++ sources/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        obtainMusicDownloadUrl()
     }
 
-
+    func obtainMusicDownloadUrl() {
+        guard let url = URL.init(string: "https://api.imjad.cn/cloudmusic/?type=song&id=28012031&br=128000") else { return }
+        Alamofire.request(url).responseJSON { dataResponse in
+            if let statusCode = dataResponse.response?.statusCode {
+                print("http response status code \(statusCode)")
+            }
+            dataResponse.result.ifSuccess {
+                if let data = dataResponse.data, let utf8Str = String(data: data, encoding: String.Encoding.utf8) {
+                    print("json result ===> \(utf8Str)")
+                }
+            }
+            dataResponse.result.ifFailure {
+                if let error = dataResponse.error {
+                    print("error description ===> \(error.localizedDescription)")
+                }
+            }
+        }
+    }
 }
 
